78763fa751121b09531b258a9e1084e13d736194
finn1111 <finn.roessler.05@gmail.com>
2022-07-13 15:49:07 +0200

    LKW_40 verbrauch

--- src/main/java/org/example/BO/LKW_40.java
+++ src/main/java/org/example/BO/LKW_40.java
@@ -1,4 +1,39 @@
 package org.example.BO;
 
-public class LKW_40  extends AbstractFahrzeug {
+public class LKW_40  implements Verbrauchprostunde {
+
+
+
+
+        public float verbrauchkwh, fahrzeitinstunden;
+
+        public LKW_40() {
+        }
+
+        public LKW_40 (float verbrauchkwh, float fahrzeitinstunden)
+        {
+            this.verbrauchkwh = verbrauchkwh;
+            this.fahrzeitinstunden = fahrzeitinstunden;
+        }
+
+        public float getverbrauchkwh() {
+            return verbrauchkwh;
+        }
+
+
+        public float getfahrzeitinstunden() {
+            return fahrzeitinstunden;
+        }
+
+        public float getverbrauch()
+        {
+            return
+                    verbrauchkwh / fahrzeitinstunden ;
+        }
+
+
+
+
+
+
 }
